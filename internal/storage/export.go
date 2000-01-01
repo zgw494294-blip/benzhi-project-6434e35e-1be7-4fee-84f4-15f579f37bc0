6755b5c0b8df7951c6d5637c9c9c566714ecef83
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Green repair and reproduction for 6434e35e-1be7-4fee-84f4-15f579f37bc0-bug-4

--- internal/storage/export.go
+++ internal/storage/export.go
@@ -4,6 +4,7 @@ import (
 	"archive-deacidification/internal/domain"
 	"context"
 	"encoding/json"
+	"fmt"
 )
 
 type EvidencePackage struct {
@@ -38,9 +39,56 @@ func (s *Store) ImportEvidence(ctx context.Context, raw []byte) error {
 	if d, e := domain.EvidenceDigest(&p.Batch); e != nil || d != p.Digest {
 		return domain.ErrInvalidMetrics
 	}
+	if e := verifyPackageEvents(p.Batch.BatchID, p.Events); e != nil {
+		return fmt.Errorf("%w: %s", domain.ErrAuditChain, e.Error())
+	}
 	s.mu.Lock()
 	defer s.mu.Unlock()
+	if _, exists := s.data.Batches[p.Batch.BatchID]; exists {
+		return domain.ErrVersionConflict
+	}
+	if hasEventsLocked(s.data.Events, p.Batch.BatchID) {
+		return domain.ErrVersionConflict
+	}
+	batchBefore, batchExisted := s.data.Batches[p.Batch.BatchID]
+	prevEventCount := len(s.data.Events)
 	s.data.Batches[p.Batch.BatchID] = p.Batch
 	s.data.Events = append(s.data.Events, p.Events...)
+	if e := s.verifyChainLocked(p.Batch.BatchID); e != nil {
+		s.data.Batches[p.Batch.BatchID] = batchBefore
+		if !batchExisted {
+			delete(s.data.Batches, p.Batch.BatchID)
+		}
+		s.data.Events = s.data.Events[:prevEventCount]
+		return e
+	}
 	return s.persist()
+}
+
+// verifyPackageEvents validates that the events in an evidence package form a
+// contiguous, untampered audit chain belonging to the claimed batch. It rejects
+// forged digests, misordered sequences, and events sourced from other batches.
+func verifyPackageEvents(batchID string, events []AuditEvent) error {
+	prev := ""
+	for index, event := range events {
+		if event.BatchID != batchID {
+			return fmt.Errorf("事件 %d 的 batchID %q 与批次 %q 不一致", event.Seq, event.BatchID, batchID)
+		}
+		if event.Seq != int64(index+1) || event.PrevDigest != prev || event.Digest != eventDigest(event) {
+			return fmt.Errorf("事件 %d 不连续或摘要不符", event.Seq)
+		}
+		prev = event.Digest
+	}
+	return nil
+}
+
+// hasEventsLocked reports whether any event in the global journal already
+// belongs to the given batch. It must be called with s.mu held.
+func hasEventsLocked(events []AuditEvent, batchID string) bool {
+	for _, event := range events {
+		if event.BatchID == batchID {
+			return true
+		}
+	}
+	return false
 }
